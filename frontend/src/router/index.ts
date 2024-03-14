6bb813c107dfe61d02f297d0b8b581a40b245dc4
Arnau Badenas <4bvives@gmail.com>
2024-03-14 15:37:53 +0100

    pwa, vite config update

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,20 +1,16 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '',
-    redirect: '/folder/Inbox'
-  },
-  {
-    path: '/folder/:id',
-    component: () => import ('../views/FolderPage.vue')
-  }
-]
+	{
+		path: "",
+		redirect: "/folder/Inbox",
+	},
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
+});
 
-export default router
+export default router;
