79d7e88eb87b1b3325a1788cecb2defb40d6ca86
Arnau Badenas <4bvives@gmail.com>
2024-04-09 17:41:45 +0200

    Menu responsive funcional Hacer un placeholder del menu, es responsive y funciona pero vendria bien adaptar mejor el diseño.

--- frontend/src/components/AppMenu.vue
+++ frontend/src/components/AppMenu.vue
@@ -1,6 +1,6 @@
 <template>
 	<ion-page>
-		<ion-tabs>
+		<ion-tabs class="desktop-menu">
 			<ion-router-outlet></ion-router-outlet>
 			<ion-tab-bar slot="top">
 				<ion-tab-button tab="home" href="/home">
@@ -11,6 +11,32 @@
 				</ion-tab-button>
 			</ion-tab-bar>
 		</ion-tabs>
+		<div class="mobile-menu">
+			<ion-menu content-id="main-content">
+				<ion-header>
+					<ion-toolbar>
+						<ion-title>Escoge la opcion</ion-title>
+					</ion-toolbar>
+				</ion-header>
+				<ion-content class="ion-padding">
+					<ion-button router-link="/">Home</ion-button>
+					<ion-button router-link="/adopta">Adopta</ion-button>
+				</ion-content>
+			</ion-menu>
+			<ion-page id="main-content">
+				<ion-header>
+					<ion-toolbar>
+						<ion-buttons slot="start">
+							<ion-menu-button></ion-menu-button>
+						</ion-buttons>
+						<ion-title>Menu</ion-title>
+					</ion-toolbar>
+				</ion-header>
+				<ion-content class="ion-padding">
+					<ion-router-outlet></ion-router-outlet>
+				</ion-content>
+			</ion-page>
+		</div>
 	</ion-page>
 </template>
 
@@ -22,5 +48,32 @@ import {
 	IonTabBar,
 	IonTabButton,
 	IonLabel,
+	IonMenu,
+	IonHeader,
+	IonToolbar,
+	IonButtons,
+	IonMenuButton,
+	IonTitle,
+	IonContent,
+	IonButton,
 } from "@ionic/vue";
 </script>
+
+<style lang="scss">
+@media (max-width: 768px) {
+	.desktop-menu {
+		display: none;
+	}
+	.mobile-menu {
+		display: block;
+	}
+}
+@media (min-width: 768px) {
+	.desktop-menu {
+		display: block;
+	}
+	.mobile-menu {
+		display: none;
+	}
+}
+</style>
