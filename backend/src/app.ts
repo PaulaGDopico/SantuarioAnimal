c6d1a13123b3e32bf20fb89b9d2175769024654d
Arnau Badenas <4bvives@gmail.com>
2024-04-23 19:02:29 +0200

    login y signup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import habitacionRoutes from "./routes/habitacionRoutes";
 import animalRoutes from "./routes/animalRoutes";
+import authRoutes from "./routes/authRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.json());
+app.use(express.json());
 app.use(express.static("public"));
 
 //All routes
 app.use("/habitaciones", habitacionRoutes);
 app.use("/animales", animalRoutes);
+app.use("/user", authRoutes);
 
 //External commands
 app.listen(PORT, () => {
