e905b8e2e989cb9c1fab7ed06ff122d7f509e7a8
Paula Garcia <paula.gdopico@gmail.com>
2024-04-25 19:44:59 +0200

    cambios y nuevos componentes que seguramente acabe borrando

---
+++ frontend/src/components/GestionarAnimalesEditButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <ion-button @click="setOpenSecond(true)">Edit</ion-button>
+    <!-- MODAL MODIFICAR -->
+    <ion-modal :is-open="isOpenSecond">
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Modal</ion-title>
+                <ion-buttons slot="end">
+                    <ion-button @click="setOpenSecond(false)">Close</ion-button>
+                </ion-buttons>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum quidem recusandae ducimus quos
+                reprehenderit. Veniam, molestias quos, dolorum consequuntur nisi deserunt omnis id illo sit cum qui.
+                Eaque,
+                dicta.
+            </p>
+        </ion-content>
+    </ion-modal>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { IonButtons, IonButton, IonModal, IonHeader, IonToolbar, IonContent, IonTitle } from '@ionic/vue';
+
+const isOpenSecond = ref(false);
+const setOpenSecond = (open: boolean) => {
+    isOpenSecond.value = open
+    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAa')
+};
+
+</script>
--- frontend/src/views/GestionarAnimales.vue
+++ frontend/src/views/GestionarAnimales.vue
@@ -6,9 +6,7 @@
           <ion-row class="ion-justify-content-between ion-align-items-center">
             <ion-col>
               <ion-button>
-                <RouterLink :to="{
-                  name: 'InicioSesion',
-                }">Volver Atras</RouterLink>
+                <RouterLink :to="{ name: 'InicioSesion'}">Volver Atras</RouterLink>
               </ion-button>
             </ion-col>
             <ion-col>
@@ -21,7 +19,7 @@
         </ion-grid>
         <ag-grid-vue :rowData="rowData" :columnDefs="columnDefs" style="height: 500px" class="ag-theme-quartz">
         </ag-grid-vue>
-        <!-- MODAL MODIFICAR -->
+        <!-- MODAL CREAR -->
         <ion-modal :is-open="isOpen">
           <ion-header>
             <ion-toolbar>
@@ -40,31 +38,13 @@
             </p>
           </ion-content>
         </ion-modal>
-        <!-- MODAL MODIFICAR -->
-        <ion-modal :is-open="isOpenSecond">
-          <ion-header>
-            <ion-toolbar>
-              <ion-title>Modal</ion-title>
-              <ion-buttons slot="end">
-                <ion-button @click="setOpenSecond(false)">Close</ion-button>
-              </ion-buttons>
-            </ion-toolbar>
-          </ion-header>
-          <ion-content class="ion-padding">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum quidem recusandae ducimus quos
-              reprehenderit. Veniam, molestias quos, dolorum consequuntur nisi deserunt omnis id illo sit cum qui.
-              Eaque,
-              dicta.
-            </p>
-          </ion-content>
-        </ion-modal>
+        
       </div>
       <app-footer></app-footer>
     </ion-content>
   </ion-page>
 </template>
-<script lang="ts">
+<script setup lang="ts">
 import {
   IonContent,
   IonItem,
@@ -81,108 +61,49 @@ import {
   IonCol
 } from "@ionic/vue";
 import { ref } from 'vue';
-import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
-import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 import { AgGridVue } from "ag-grid-vue3"; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; 
 import AppFooter from '@/components/AppFooter.vue';
+import GestionarAnimalesEditButton from "@/components/GestionarAnimalesEditButton.vue";
 
-export default {
-  components: {
-    AgGridVue,
-    IonContent,
-    IonItem,
-    IonModal,
-    IonButton,
-    IonButtons,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonInput,
-    IonPage,
-    AppFooter,
-    IonGrid,
-    IonRow,
-    IonCol
-  },
-  data() {
-    return {
-      columnDefs: [
-        { headerName: 'Nombre', field: 'nombre', sortable: true, filter: true },
-        { headerName: 'Tipo', field: 'tipo', sortable: true, filter: true },
-        { headerName: 'Sexo', field: 'sexo', sortable: true, filter: true },
-        { headerName: 'Raza', field: 'raza', sortable: true, filter: true },
-        { headerName: 'Estado', field: 'estado', sortable: true, filter: true },
-        { headerName: 'Imagen', cellRenderer: imageRenderer },
-        { headerName: 'Acción', cellRenderer: actionRenderer } // Cambiado de 'cellRendererFramework' a 'cellRenderer'
-      ],
-      rowData: [
-        { nombre: 'Roy', tipo: 'Perro', sexo: 'Macho', raza: 'Bulldog', estado: 'casos-especiales', imagen: 'pexels-snapwire-46024.jpg' },
-        { nombre: 'Marin', tipo: 'Gato', sexo: 'Hembra', raza: 'Persa', estado: 'sin-estado', imagen: 'pexels-snapwire-46024.jpg' },
-        { nombre: 'Gafe', tipo: 'Gato', sexo: 'Macho', raza: 'Europeo', estado: 'adopcion-urgente', imagen: 'pexels-snapwire-46024.jpg' },
-        { nombre: 'Tristepin', tipo: 'Perro', sexo: 'Hembra', raza: 'Chihuahua', estado: 'apadrinado', imagen: 'pexels-snapwire-46024.jpg' }
-      ]
-    };
-  },
-  setup() {
-    const message = ref('This modal example uses triggers to automatically open a modal when the button is clicked.');
-    const modal = ref();
-    const input = ref();
+// BOTONES MODAL
+const isOpen = ref(false);
+const setOpen = (open: boolean) => (isOpen.value = open);
 
-    const cancel = () => modal.value.$el.dismiss(null, 'cancel');
+const columnDefs = ref([
+  { headerName: 'Nombre', field: 'nombre', sortable: true, filter: true },
+  { headerName: 'Tipo', field: 'tipo', sortable: true, filter: true },
+  { headerName: 'Sexo', field: 'sexo', sortable: true, filter: true },
+  { headerName: 'Raza', field: 'raza', sortable: true, filter: true },
+  { headerName: 'Estado', field: 'estado', sortable: true, filter: true },
+  { headerName: 'Imagen', cellRenderer: imageRenderer },
+  { headerName: 'Acción', field: 'edit',cellRenderer: GestionarAnimalesEditButton }
+]);
 
-    const confirm = () => {
-      const name = input.value.$el.value;
-      modal.value.$el.dismiss(name, 'confirm');
-    };
+const rowData = ref([
+  { nombre: 'Roy', tipo: 'Perro', sexo: 'Macho', raza: 'Bulldog', estado: 'casos-especiales', imagen: 'pexels-snapwire-46024.jpg'},
+  // { nombre: 'Marin', tipo: 'Gato', sexo: 'Hembra', raza: 'Persa', estado: 'sin-estado', imagen: 'pexels-snapwire-46024.jpg' },
+  // { nombre: 'Gafe', tipo: 'Gato', sexo: 'Macho', raza: 'Europeo', estado: 'adopcion-urgente', imagen: 'pexels-snapwire-46024.jpg' },
+  // { nombre: 'Tristepin', tipo: 'Perro', sexo: 'Hembra', raza: 'Chihuahua', estado: 'apadrinado', imagen: 'pexels-snapwire-46024.jpg'}
+]);
 
-    const onWillDismiss = (ev: any) => {
-      if (ev.detail.role === 'confirm') {
-        message.value = `Hello, ${ev.detail.data}!`;
-      }
-    };
-    const isOpen = ref(false);
-    const isOpenSecond = ref(false);
-
-    const setOpen = (open: boolean) => (isOpen.value = open);
-    const setOpenSecond = (open: boolean) => (isOpen.value = open);
-    return {
-      message,
-      modal,
-      input,
-      cancel,
-      confirm,
-      onWillDismiss,
-      isOpen,
-      setOpen,
-      isOpenSecond,
-      setOpenSecond
-    };
-  }
-};
-
-
-// Función para renderizar el contenido de la celda como botones
-function actionRenderer(params:any) {
+function actionRenderer(params: any) {
   const wrapper = document.createElement('div');
 
-  // Botón 1
-  const button1 = document.createElement('ion-button');
-  button1.setAttribute("click", "setOpenSecond(false)")
+  const button1 = document.createElement('button');
+  button1.setAttribute("onclick", "setOpenSecond(false)")
   button1.innerHTML = 'Edit';
   wrapper.appendChild(button1);
 
-  // Botón 2
   const button2 = document.createElement('ion-button');
-  button2.setAttribute("id", "open-modal")
   button2.innerHTML = 'Del';
   button2.addEventListener('click', () => {
-    const confirmation = confirm(`¿Estás seguro de que deseas eliminar a ${params.data.nombre}?`);
+    const confirmation = window.confirm(`¿Estás seguro de que deseas eliminar a ${params.data.nombre}?`);
     console.log(params.data.nombre)
     if (confirmation) {
-      // La acción de eliminar se realizará si se selecciona "Aceptar"
       console.log("Eliminando el elemento...");
     } else {
-      // No se realiza ninguna acción si se selecciona "Cancelar"
       console.log("Eliminación cancelada.");
     }
   });
@@ -191,15 +112,13 @@ function actionRenderer(params:any) {
   return wrapper;
 }
 
-// Función para renderizar el contenido de la celda como imágenes
 function imageRenderer(params: any) {
   const wrapper = document.createElement('div');
 
-  // Imagen
   const image = document.createElement('img');
-  image.src += '/img/' + params.data.imagen; // URL de la imagen // Texto alternativo de la imagen
-  image.style.width = '100px'; // Estilo opcional
-  image.style.height = '100px'; // Estilo opcional
+  image.src += '/img/' + params.data.imagen;
+  image.style.width = '100px';
+  image.style.height = '100px';
   wrapper.appendChild(image);
 
   return wrapper;
