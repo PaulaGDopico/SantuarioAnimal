346d65259da05920beb151b72860bbe272820f6b
Arnau Badenas <4bvives@gmail.com>
2024-05-03 20:21:14 +0200

    formulario para adopta y apadrina completo

--- frontend/src/components/InfoAnimalAdoptaModal.vue
+++ frontend/src/components/InfoAnimalAdoptaModal.vue
@@ -14,39 +14,59 @@
 	</ion-header>
 	<ion-content class="ion-padding">
 		<ion-item>
-			<ion-label position="stacked">Ingrese su nombre</ion-label>
-			<ion-input v-model="name" placeholder="Su nombre"></ion-input>
+			<ion-label position="stacked">Nombre</ion-label>
+			<ion-input v-model="name" placeholder="Tu nombre"></ion-input>
 		</ion-item>
+
 		<ion-item>
-			<ion-label position="stacked"
-				>Ingrese su correo electrónico</ion-label
-			>
+			<ion-label position="stacked">Teléfono</ion-label>
+			<ion-input v-model="phone" placeholder="Tu teléfono"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Dirección</ion-label>
+			<ion-input v-model="address" placeholder="Tu dirección"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Código Postal</ion-label>
 			<ion-input
-				v-model="email"
-				type="email"
-				placeholder="Su correo electrónico"></ion-input>
+				v-model="postalCode"
+				placeholder="Tu código postal"></ion-input>
 		</ion-item>
+
 		<ion-item>
-			<ion-label position="stacked">Seleccione su género</ion-label>
-			<ion-select v-model="gender" placeholder="Género">
-				<ion-select-option value="masculino"
-					>Masculino</ion-select-option
-				>
-				<ion-select-option value="femenino">Femenino</ion-select-option>
-				<ion-select-option value="otro">Otro</ion-select-option>
-			</ion-select>
+			<ion-label position="stacked">País</ion-label>
+			<ion-input v-model="country" placeholder="Tu país"></ion-input>
 		</ion-item>
+
 		<ion-item>
-			<ion-label position="stacked">Seleccione su ciudad</ion-label>
-			<ion-select v-model="city" placeholder="Ciudad">
-				<ion-select-option value="madrid">Madrid</ion-select-option>
-				<ion-select-option value="barcelona"
-					>Barcelona</ion-select-option
-				>
-				<ion-select-option value="valencia">Valencia</ion-select-option>
-			</ion-select>
+			<ion-label position="stacked">Edad</ion-label>
+			<ion-input v-model="age" placeholder="Tu edad"></ion-input>
 		</ion-item>
-		
+
+		<ion-item>
+			<ion-label position="stacked">Ocupación</ion-label>
+			<ion-input
+				v-model="occupation"
+				placeholder="Tu ocupación"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Experiencia con mascotas</ion-label>
+			<ion-textarea
+				v-model="experience"
+				placeholder="Tu experiencia con mascotas"></ion-textarea>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Motivo de adopción</ion-label>
+			<ion-textarea
+				v-model="adoptionReason"
+				placeholder="Tu motivo para adoptar"></ion-textarea>
+		</ion-item>
+
+
 	</ion-content>
 </template>
 
@@ -60,28 +80,39 @@ import {
 	IonButtons,
 	IonButton,
 	IonItem,
-	IonInput,
 	IonLabel,
-	IonSelect,
-	IonSelectOption,
+	IonInput,
+	IonTextarea,
 	modalController,
 } from "@ionic/vue";
 import { ref } from "vue";
-const name = ref<string>("");
-const email = ref<string>("");
-const gender = ref<string>("");
-const city = ref<string>("");
+
+const name = ref("");
+const phone = ref("");
+const address = ref("");
+const postalCode = ref("");
+const country = ref("");
+const age = ref("");
+const occupation = ref("");
+const experience = ref("");
+const adoptionReason = ref("");
 
 const cancel = () => modalController.dismiss(null, "cancel");
-const confirm = () => {
-	const formData = {
-		name: name.value,
-		email: email.value,
-		gender: gender.value,
-		city: city.value,
-	};
-	modalController.dismiss(formData, "confirm");
-};
+const confirm = () =>
+	modalController.dismiss(
+		{
+			name: name.value,
+			phone: phone.value,
+			address: address.value,
+			postalCode: postalCode.value,
+			country: country.value,
+			age: age.value,
+			occupation: occupation.value,
+			experience: experience.value,
+			adoptionReason: adoptionReason.value,
+		},
+		"confirm"
+	);
 </script>
 
 <style scoped lang="scss">
--- frontend/src/components/InfoAnimalApadrinaModal.vue
+++ frontend/src/components/InfoAnimalApadrinaModal.vue
@@ -14,11 +14,56 @@
 	</ion-header>
 	<ion-content class="ion-padding">
 		<ion-item>
+			<ion-label position="stacked">Nombre</ion-label>
+			<ion-input v-model="name" placeholder="Tu nombre"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Teléfono</ion-label>
+			<ion-input v-model="phone" placeholder="Tu teléfono"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Dirección</ion-label>
+			<ion-input v-model="address" placeholder="Tu dirección"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Código Postal</ion-label>
 			<ion-input
-				label-placement="stacked"
-				label="Enter your name"
-				v-model="name"
-				placeholder="Your name"></ion-input>
+				v-model="postalCode"
+				placeholder="Tu código postal"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">País</ion-label>
+			<ion-input v-model="country" placeholder="Tu país"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Edad</ion-label>
+			<ion-input v-model="age" placeholder="Tu edad"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Ocupación</ion-label>
+			<ion-input
+				v-model="occupation"
+				placeholder="Tu ocupación"></ion-input>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Experiencia con mascotas</ion-label>
+			<ion-textarea
+				v-model="experience"
+				placeholder="Tu experiencia con mascotas"></ion-textarea>
+		</ion-item>
+
+		<ion-item>
+			<ion-label position="stacked">Motivo de adopción</ion-label>
+			<ion-textarea
+				v-model="adoptionReason"
+				placeholder="Tu motivo para adoptar"></ion-textarea>
 		</ion-item>
 	</ion-content>
 </template>
@@ -33,14 +78,39 @@ import {
 	IonButtons,
 	IonButton,
 	IonItem,
+	IonLabel,
 	IonInput,
+	IonTextarea,
 	modalController,
 } from "@ionic/vue";
 import { ref } from "vue";
-const name = ref();
+
+const name = ref("");
+const phone = ref("");
+const address = ref("");
+const postalCode = ref("");
+const country = ref("");
+const age = ref("");
+const occupation = ref("");
+const experience = ref("");
+const adoptionReason = ref("");
 
 const cancel = () => modalController.dismiss(null, "cancel");
-const confirm = () => modalController.dismiss(name.value, "confirm");
+const confirm = () =>
+	modalController.dismiss(
+		{
+			name: name.value,
+			phone: phone.value,
+			address: address.value,
+			postalCode: postalCode.value,
+			country: country.value,
+			age: age.value,
+			occupation: occupation.value,
+			experience: experience.value,
+			adoptionReason: adoptionReason.value,
+		},
+		"confirm"
+	);
 </script>
 
 <style scoped lang="scss">
