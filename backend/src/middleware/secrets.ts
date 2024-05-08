2d6570a10b1a14999b1006d5fe252c2965f93e5d
Arnau Badenas <4bvives@gmail.com>
2024-05-08 15:13:14 +0200

    asdasd

--- backend/src/middleware/secrets.ts
+++ backend/src/middleware/secrets.ts
@@ -1,5 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "../../.env" });
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET!;
+//export const API_FILE_URL = "https://game-perfect-ant.ngrok-free.app/uploads/"
+export const API_FILE_URL = `https://localhost:${PORT}/uploads/`;
+
+export const MAIL_API_KEY = process.env.MAIL_API_KEY!;
+export const MAIL_DOMAIN = process.env.MAIL_DOMAIN!;
