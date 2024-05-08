a86bf05d8726608b40522d12b3735c51d1962c8c
Arnau Badenas <4bvives@gmail.com>
2024-05-08 15:55:57 +0200

    cors backend config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,9 +11,13 @@ var cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+	origin: 'https://santuario-animal-frontend.vercel.app',
+ };
+
 app.use(express.json());
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors(corsOptions));
 
 //All routes
 
