d5ff801a99a4df5aa101770c07f790322bc15d3e
Arnau Badenas <4bvives@gmail.com>
2024-05-09 00:15:07 +0200

    images

--- frontend/src/middleware/secrets.ts
+++ frontend/src/middleware/secrets.ts
@@ -2,4 +2,5 @@
 // dotenv.config({path: "../../.env"})
 //export const API_URL = "https://game-perfect-ant.ngrok-free.app/api";
 export const API_URL = "https://santuario-animal.vercel.app";
-export const API_FILE_URL = "https://santuario-animal.vercel.app";
+export const API_FILE_URL =
+    "https://q4grcdwffd4aa6wn.public.blob.vercel-storage.com/";
