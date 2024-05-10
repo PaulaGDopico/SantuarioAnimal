27125d701a900811f17f3be87449e0c1a94d059d
Arnau Badenas <4bvives@gmail.com>
2024-05-10 10:05:16 +0200

    añadir registrar administrador rutas y menu

---
+++ frontend/src/views/RegistrarAdministradorView.vue
@@ -0,0 +1,12 @@
+<template>
+    <ion-page>
+        <ion-content>
+            <h1>Registrar administrador</h1>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonPage } from "@ionic/vue";
+</script>
+<style scoped lang="scss"></style>
