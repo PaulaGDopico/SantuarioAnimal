df2de57bb6d9cf999d34eebe34f1b508e4d60880
Arnau Badenas <4bvives@gmail.com>
2024-05-10 17:07:32 +0200

    migrations

--- frontend/src/services/animal.ts
+++ frontend/src/services/animal.ts
@@ -9,9 +9,7 @@ import {
 export const getAnimal = async (animalId: string) => {
     try {
         console.log(API_URL + "/animales/animal/" + animalId);
-        const response = await fetch(
-            API_URL + "/animales/animal/" + animalId,
-        );
+        const response = await fetch(API_URL + "/animales/animal/" + animalId);
         const animal: Animal = await response.json();
         console.log(animal);
         // Formatear fechas
@@ -38,9 +36,7 @@ export const getAnimal = async (animalId: string) => {
 
 export const getAnimales = async (numeroPagina: number) => {
     try {
-        const response = await fetch(
-            API_URL + "/animales/" + numeroPagina,
-        );
+        const response = await fetch(API_URL + "/animales/" + numeroPagina);
         const animales: Array<Animal> = await response.json();
         console.log(animales);
         return animales;
@@ -59,68 +55,58 @@ export const getAllAnimalsWithoutPagination = async () => {
     }
 };
 
-export const pushAnimal = async (animalData:Animal) => {
-	try{
-		const formData = new FormData
-		
-		formData.append('createdAt', animalData.createdAt);
-		formData.append('updatedAt', animalData.updatedAt);
-		formData.append('nombre', animalData.nombre);
-		formData.append('tipo', animalData.tipo);
-		formData.append('estado_adopcion', animalData.estado_adopcion);
-		formData.append('peso', animalData.peso);
-		formData.append('tamanyo', animalData.tamanyo);
-		formData.append('raza', animalData.raza);
-		formData.append('fecha_nacimiento', animalData.fecha_nacimiento);
-		formData.append('fecha_ingreso', animalData.fecha_ingreso);
-		formData.append('sexo', animalData.sexo);
-		formData.append('image', animalData.img);
-		formData.append('descripcion', animalData.descripcion ?? '');
-		formData.append('habitacionId', animalData.habitacionId.toString());
-		if (animalData.donaciones_recibidas) {
-			formData.append('donaciones_recibidas', JSON.stringify(animalData.donaciones_recibidas));
-		} else {
-			formData.append('donaciones_recibidas', '');
-		}
-		formData.append('afiliadoId', animalData.afiliadoId !== null ? animalData.afiliadoId.toString() : '');
+export const pushAnimal = async (animalData: Animal) => {
+    try {
+        const formData = new FormData();
 
-		const response = await fetch(
-			'http://localhost:3000/animales',
-			{
-				method: 'POST',
-				body: formData
-			}
-		)
-		if (response.ok) {
+        formData.append("createdAt", animalData.createdAt);
+        formData.append("updatedAt", animalData.updatedAt);
+        formData.append("nombre", animalData.nombre);
+        formData.append("tipo", animalData.tipo);
+        formData.append("estado_adopcion", animalData.estado_adopcion);
+        formData.append("peso", animalData.peso);
+        formData.append("tamanyo", animalData.tamanyo);
+        formData.append("raza", animalData.raza);
+        formData.append("fecha_nacimiento", animalData.fecha_nacimiento);
+        formData.append("fecha_ingreso", animalData.fecha_ingreso);
+        formData.append("sexo", animalData.sexo);
+        formData.append("image", animalData.img);
+        formData.append("descripcion", animalData.descripcion ?? "");
+        formData.append("habitacionId", animalData.habitacionId.toString());
+        if (animalData.donaciones_recibidas) {
+            formData.append(
+                "donaciones_recibidas",
+                JSON.stringify(animalData.donaciones_recibidas)
+            );
+        } else {
+            formData.append("donaciones_recibidas", "");
+        }
+        formData.append(
+            "afiliadoId",
+            animalData.afiliadoId !== null
+                ? animalData.afiliadoId.toString()
+                : ""
+        );
+
+        const response = await fetch("http://localhost:3000/animales", {
+            method: "POST",
+            body: formData,
+        });
+        if (response.ok) {
             const data = await response.json();
             // Manejar la respuesta exitosa, como mostrar un mensaje de éxito o redirigir a una página de inicio de sesión
-            console.info('Subida exitoso:', data);
+            console.info("Subida exitoso:", data);
             //location.reload()
-		}
-		if (!response.ok) {
-            throw new Error('Error al intentar subir el animal.');
         }
-		return true
-	}catch(error){
-		console.error('Error al subir el animal:', error);
+        if (!response.ok) {
+            throw new Error("Error al intentar subir el animal.");
+        }
+        return true;
+    } catch (error) {
+        console.error("Error al subir el animal:", error);
         return false;
-	}
-}
-
-
-export const deleteAnimal = async (id:number) =>{
-	try{
-		const response = await fetch(
-			`http://localhost:3000/animales/${id}`,
-			{
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-		)
-		if (!response.ok) {
-            throw new Error('Error al intentar eliminar el animal.');
+    }
+};
 
 export const deleteAnimal = async (id: number) => {
     try {
@@ -133,7 +119,6 @@ export const deleteAnimal = async (id: number) => {
 
         if (!response.ok) {
             throw new Error("Error al intentar eliminar el animal.");
-
         }
         return true;
     } catch (error) {
