fdd9c3cf05d6f8818d9da5f4ac404b31d0d966f6
Arnau Badenas <4bvives@gmail.com>
2024-05-10 10:28:05 +0200

    formulario de registro funcional

--- frontend/src/views/RegistrarAdministradorView.vue
+++ frontend/src/views/RegistrarAdministradorView.vue
@@ -1,12 +1,95 @@
 <template>
     <ion-page>
         <ion-content>
-            <h1>Registrar administrador</h1>
+            <section class="formularioAdministrador">
+                <section class="container ion-padding">
+                    <form
+                        action=""
+                        @submit.prevent="
+                            enviarFormulario(email, password, nombre, telefono)
+                        ">
+                        <ion-grid>
+                            <ion-row>
+                                <ion-col>
+                                    <h2>Añadir un administrador</h2>
+                                </ion-col>
+                            </ion-row>
+                            <ion-row size="12" size-sm="6">
+                                <ion-col>
+                                    <ion-input
+                                        label="Email"
+                                        label-placement="floating"
+                                        fill="solid"
+                                        placeholder="Introduce tu email"
+                                        v-model="email"></ion-input>
+                                </ion-col>
+                                <ion-col size="12" size-sm="6">
+                                    <ion-input
+                                        label="Contraseña"
+                                        label-placement="floating"
+                                        fill="solid"
+                                        placeholder="Introduce tu contraseña"
+                                        v-model="password"></ion-input>
+                                </ion-col>
+
+                                <ion-col size="12" size-sm="6">
+                                    <ion-input
+                                        label="Nombre"
+                                        label-placement="floating"
+                                        fill="solid"
+                                        placeholder="Introduce tu nombre"
+                                        v-model="nombre"></ion-input>
+                                </ion-col>
+
+                                <ion-col size="12" size-sm="6">
+                                    <ion-input
+                                        label="Teléfono"
+                                        label-placement="floating"
+                                        fill="solid"
+                                        placeholder="Introduce tu teléfono"
+                                        v-model="telefono"></ion-input>
+                                </ion-col>
+                            </ion-row>
+
+                            <ion-row>
+                                <ion-col>
+                                    <ion-button type="submit"
+                                        >Solicitar unirse</ion-button
+                                    >
+                                </ion-col>
+                            </ion-row>
+                        </ion-grid>
+                    </form>
+                </section>
+            </section>
         </ion-content>
     </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonPage } from "@ionic/vue";
+import { register } from "@/services/auth";
+import {
+    IonContent,
+    IonPage,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonButton,
+    IonInput,
+} from "@ionic/vue";
+
+const email: string = "";
+const password: string = "";
+const nombre: string = "";
+const telefono: string = "";
+
+async function enviarFormulario(
+    email: string,
+    password: string,
+    nombre: string,
+    telefono: string
+) {
+    register(email, password, nombre, telefono);
+}
 </script>
 <style scoped lang="scss"></style>
