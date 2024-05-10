fb0adf9e57d689e106594cbe221c64d18c1c4f17
Arnau Badenas <4bvives@gmail.com>
2024-05-10 07:56:18 +0200

    actualizar ruta animales

--- frontend/src/middleware/secrets.ts
+++ frontend/src/middleware/secrets.ts
@@ -1,5 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config({path: "../../.env"})
-//export const API_URL = "https://game-perfect-ant.ngrok-free.app/api";
+
 export const API_URL = "https://santuario-animal.vercel.app";
 export const API_FILE_URL = "https://santuario-animal.vercel.app/";
