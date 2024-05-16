4f6e542ff856c67940d007016ec0b4b9b5944684
Arnau Badenas <4bvives@gmail.com>
2024-05-16 17:01:02 +0200

    afiliado se pone en la base de datos cuando se inscribe

--- frontend/src/services/afiliado.ts
+++ frontend/src/services/afiliado.ts
@@ -5,10 +5,37 @@ export const getAfiliados = async (): Promise<Array<Afiliado>> => {
     try {
         const url = API_URL + "/afiliado";
         const response = await fetch(url);
-        const afiliados: Array<Afiliado> = await response.json();
-        console.log(response);
-        return afiliados;
+        return await response.json();
     } catch (error) {
         throw new Error("Afiliados no encontrados")
     }
 };
+
+interface DatosAfiliado {
+    nombre: string,
+    apellidos: string,
+    telefono: string,
+    email: string,
+}
+
+export const postAfiliado = async (datosAfiliado: DatosAfiliado) => {
+    try {
+        const url = API_URL + "/afiliado";
+        const afiliado = {
+            "nombre": datosAfiliado.nombre + " " + datosAfiliado.apellidos,
+            "telefono": datosAfiliado.telefono,
+            "email": datosAfiliado.email
+        }
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(afiliado)
+        })
+        return await response.json()
+    } catch (error) {
+        throw new Error("No se ha podido añadir el afiliado")
+    }
+}
